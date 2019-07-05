fbc6cfaf30f36ccf445f87a0d49b5840d101858b
Kristjan Tampere <dacmd@mail.ru>
2019-07-05 20:52:21 +0300

    commit

---
+++ src/main/java/com/imageblog/imageblog/ImageBlogApplication.java
@@ -0,0 +1,13 @@
+package com.imageblog.imageblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageBlogApplication.class, args);
+    }
+
+}
